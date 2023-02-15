394f1381a99212ef037ade3ed589428f0c090238
IvanNikolic21 <ivan.nikolic@sns.it>
2023-02-15 12:00:26 +0100

    added uvlf till the end, forest calculation and started Planck

--- test.py
+++ test.py
@@ -64,6 +64,72 @@ global_params = {
     'PhotonConsEndCalibz':3.5
 }
 
+### tau_GP function is the exact replica of 21cmmc.
+def tau_GP(self, gamma_bg, delta, temp, redshifts, cosmo_params):
+    r"""Calculating the lyman-alpha optical depth in each pixel using the fluctuating GP approximation.
+    Parameters
+    ----------
+    gamma_bg : float or array_like
+        The background photonionization rate in units of 1e-12 s**-1
+    delta : float or array_like
+        The underlying overdensity
+    temp : float or array_like
+        The kinectic temperature of the gas in 1e4 K
+    redshifts : float or array_like
+        Correspoding redshifts along the los
+    """
+    gamma_local = np.zeros_like(gamma_bg)
+    residual_xHI = np.zeros_like(gamma_bg, dtype=np.float64)
+
+    flag_neutral = gamma_bg == 0
+    flag_zerodelta = delta == 0
+
+    if gamma_bg.shape != redshifts.shape:
+        redshifts = np.tile(redshifts, (*gamma_bg.shape[:-1], 1))
+
+    delta_ss = (
+            2.67e4 * temp ** 0.17 * (1.0 + redshifts) ** -3 * gamma_bg ** (
+                2.0 / 3.0)
+    )
+    gamma_local[~flag_neutral] = gamma_bg[~flag_neutral] * (
+            0.98
+            * (
+                    (1.0 + (delta[~flag_neutral] / delta_ss[
+                        ~flag_neutral]) ** 1.64)
+                    ** -2.28
+            )
+            + 0.02 * (1.0 + (
+                delta[~flag_neutral] / delta_ss[~flag_neutral])) ** -0.84
+    )
+
+    Y_He = 0.245
+    # TODO: use global_params
+    residual_xHI[~flag_zerodelta] = 1 + gamma_local[
+        ~flag_zerodelta] * 1.0155e7 / (
+                                            1.0 + 1.0 / (4.0 / Y_He - 3)
+                                    ) * temp[~flag_zerodelta] ** 0.75 / (
+                                            delta[~flag_zerodelta] * (
+                                                1.0 + redshifts[
+                                            ~flag_zerodelta]) ** 3
+                                    )
+    residual_xHI[~flag_zerodelta] = residual_xHI[~flag_zerodelta] - np.sqrt(
+        residual_xHI[~flag_zerodelta] ** 2 - 1.0
+    )
+
+    return (
+            7875.053145028655
+            / (
+                    cosmo_params.hlittle
+                    * np.sqrt(
+                cosmo_params.OMm * (1.0 + redshifts) ** 3
+                + cosmo_params.OMl
+            )
+            )
+            * delta
+            * (1.0 + redshifts) ** 3
+            * residual_xHI
+    )
+
 import py21cmfast as p21c
 
 my_cache='/home/inikoli/lustre/run_directory/_cache'    #update this to the desired _cache directory.
@@ -98,7 +164,22 @@ lf_zs = [6, 7, 8, 10]
 forest_zs = [5.4, 5.6, 5.8, 6.0] # note the change in redshifts
 coeval_zs = [5,6,7,8,9,10]
 container = None
-n_muv_bins = 100
+
+n_muv_bins = 100 # for uvlf
+n_realization = 150 #for forest
+observation="bosman_optimistic"
+
+data_forest = np.load(
+        path.join(path.dirname(__file__), "data/Forests/Bosman18/data.npz"),
+        allow_pickle=True,
+    )
+
+tau_mean_CMB = 0.0569   #CMB data
+tau_sigma_u_CMB = 0.0073
+tau_sigma_l_CMB = 0.0066
+n_z_interp = 25
+z_extrap_min = 5.0
+z_extrap_max = 30.0
 
 while True:
     seed_now = np.random.randint(low=0, high=2**32-1)
@@ -220,6 +301,9 @@ while True:
             lightcone.global_xH
         )
         container.add_lightcones(lightcone)
+
+####LF part
+
     for index_uv, z_uv in enumerate(lf_zs_saved):
         mturnovers = 10 ** interp1d(np.array(lightcone.node_redshifts)[::-1], np.array(lightcone.log10_mturnovers))(
             z_uv
@@ -228,15 +312,92 @@ while True:
                 np.array(lightcone.node_redshifts)[::-1], np.array(lightcone.log10_mturnovers_mini)[::-1]
             )(z_uv)
 
-        uvlf_all = p21.compute_luminosity_function(
+        Muv, mhalo, lfunc = p21c.compute_luminosity_function(
                 mturnovers=mturnovers,
                 mturnovers_mini=mturnovers_mini,
                 redshifts=z_uv,
                 astro_params=astro_params_now,
                 flag_options=flag_options,
                 cosmo_params=cosmo_params_now,
-                user_params=self.user_params,
+                user_params=user_params,
                 nbins=n_muv_bins,
             )
 
+        Muv = [m[~np.isnan(lf)] for lf, m in zip(lfunc, Muv)]
+        mhalo = [m[~np.isnan(lf)] for lf, m in zip(lfunc, mhalo)]
+        lfunc = [m[~np.isnan(lf)] for lf, m in zip(lfunc, lfunc)]
 
+        container.add_UV((Muv, lfunc, mhalo), z_uv)
+
+####FOREST
+
+    for index_forest, z_forest in enumerate(forest_zs):
+        targets = (data_forest["zs"] > z_forest - 0.1) * (
+                data_forest["zs"] <= z_forest + 0.1
+        )
+        nlos = sum(targets)
+        bin_size = 50 / cosmo_params_now.hlittle
+
+        if lightcone is not None:
+            lightcone_redshifts = lightcone.lightcone_redshifts
+            lightcone_distances = lightcone.lightcone_distances
+            total_los = lightcone.user_params.HII_DIM ** 2
+            index_right = np.where(
+                lightcone_distances
+                > (
+                        lightcone_distances[
+                            np.where(lightcone_redshifts > z_forest)[0][
+                                0]
+                        ]
+                        + bin_size / 2
+                )
+            )[0][0]
+            index_left = np.where(
+                lightcone_distances
+                > (
+                        lightcone_distances[
+                            np.where(lightcone_redshifts > z_forest)[0][
+                                0]
+                        ]
+                        - bin_size / 2
+                )
+            )[0][0]
+            if index_left == 0:
+                index_right = np.where(
+                    lightcone_distances > (lightcone_distances[0] + bin_size)
+                )[0][0]
+
+            tau_eff = np.zeros([n_realization, nlos])
+            f_rescale_proper = 10 ** log10_f_rescale_now
+            f_rescale_proper += (z_forest-5.7) * f_rescale_slope_now
+
+            for jj in range(n_realization):
+                gamma_bg = lightcone.Gamma12_box[:, :, index_left:index_right].reshape(
+                    [total_los, index_right - index_left]
+                )[jj:: int(total_los / nlos)][: nlos]
+                delta = (
+                        lightcone.density[:, :, index_left:index_right].reshape(
+                            [total_los, index_right - index_left]
+                        )[jj:: int(total_los / nlos)][: nlos]
+                        + 1.0
+                )
+                temp = (
+                        lightcone.temp_kinetic_all_gas[:, :,
+                        index_left:index_right].reshape(
+                            [total_los, index_right - index_left]
+                        )[jj:: int(total_los / nlos)][: nlos]
+                        / 1e4
+                )
+                tau_lyman_alpha = tau_GP(
+                    gamma_bg, delta, temp,
+                    lightcone_redshifts[index_left:index_right], cosmo_params_now
+                )
+
+                tau_eff[jj] = -np.log(
+                    np.mean(np.exp(-tau_lyman_alpha * f_rescale_proper), axis=1))
+
+###FINISHED FOREST, but not saving any of it.
+
+###STARTING CMB
+    if lightcone is not None:
+        redshifts_CMB, xHI_CMB = np.sort(np.array([lightcone.node_redshifts, lightcone.global_xHI]))
